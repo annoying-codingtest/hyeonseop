6dc90c1b4ce6f43b528047b7ff73d159fca8cb18
johyunsub <gustjq2167@naver.com>
2021-09-24 00:58:15 +0900

    [Add] BOJ 2108

--- Sort/src/통계학_2108.java
+++ Sort/src/통계학_2108.java
@@ -13,23 +13,35 @@ public class 통계학_2108 {
 		for(int i=0; i<n; i++)
 			nums[i] = Integer.parseInt(br.readLine());
 		
-		//평균값
-		mean = Math.round(Arrays.stream(nums).sum());
+		// 평균값
+		mean = Math.round(Arrays.stream(nums).sum()/((float) n));
 		
-		//중앙값
+		// 중앙값
 		Arrays.sort(nums);
 		median = nums[n/2];
 		
-		//최빈값
-		int[] visit = new int[8000];
+		// 최빈값
+		int[] visit = new int[8001];	// num: -4000 ~ +4000, index: 0 ~ 8000  
 		for(int i=0; i<n; i++) 
-			visit[nums[i] + 4000]++;
-		for(int i=0; i<n; i++) {
-			
+			visit[nums[i] + 4000]++;	// 중복된 값이 있으면 카운팅
+		int max = 0;					// 최대 빈출 횟수
+		boolean flag = false;			// 최빈값 중 두 번째까지만 체크
+		for(int i=0; i<=8000; i++) {
+			if(visit[i] > max) {
+				max = visit[i];
+				mode = i - 4000;
+				flag = false;
+			}
+			else if(visit[i] == max && !flag) {
+				mode = i - 4000;
+				flag = true;
+			}
 		}
 		
-		//범위
+		// 범위
 		bound = nums[n-1] - nums[0];
-		//Arrays.stream(nums).forEach(System.out::println);
+		
+		// 출력
+		System.out.println(mean + "\n" + median + "\n" + mode + "\n" + bound);
 	}
 }
