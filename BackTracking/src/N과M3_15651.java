a02ef6145d9c90658d2f1a37a24dc3ed0194ded0
johyunsub <gustjq2167@naver.com>
2021-10-01 21:19:13 +0900

    [Add] BOJ 15651

---
+++ BackTracking/src/N과M3_15651.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N과M3_15651 {
+	static int n, m;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		int[] selected = new int[m];
+		selected[0] = 1;
+		dfs(0, selected);
+		System.out.println(sb);
+	}
+	
+	private static void dfs(int depth, int[] selected) {
+		if(depth == m) {
+			for(int s : selected)
+				sb.append(s).append(" ");
+			sb.append("\n");
+			return;
+		}
+		for(int i=1; i<=n; i++) {
+			selected[depth] = i;
+			dfs(depth + 1, selected);
+		}
+	}
+}
