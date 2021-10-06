3d19c62e9470a3849c3cb44ecd3e9163c10862af
johyunsub <gustjq2167@naver.com>
2021-10-06 23:41:26 +0900

    [Feature] Queens ArrayList (메모리초과)

--- BackTracking/src/NQueen_9663.java
+++ BackTracking/src/NQueen_9663.java
@@ -1,46 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class NQueen_9663 {
 	static int n, cnt;
+	static ArrayList<Pos> queens;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		boolean[][] v = new boolean[n][n];
-		dfs(0, v);
+		queens = new ArrayList<Pos>(); 
+		for(int col=0; col<n; col++) {
+			queens.add(new Pos(0, col));
+			dfs(1, queens);
+			queens.remove(0);
+		}
 		System.out.println(cnt);
 	}
-	
-	private static boolean[][] copyArray(boolean[][] arr) {
-		boolean[][] copiedArr = new boolean[n][n];
-		for(int i=0; i<n; i++)
-			copiedArr[i] = arr[i].clone();
-		return copiedArr;
-	}
 
-	private static void dfs(int row, boolean[][] visit) {
+	private static void dfs(int row, ArrayList<Pos> queens) {
 		if(n == row) {
 			cnt++;
 			return;
 		}
 		for(int col=0; col<n; col++) {
-			if(visit[row][col]) continue;	// 방문체크
-			boolean[][] copiedVisit = copyArray(visit);
-			CantPutQueen(row, col, copiedVisit);
-			dfs(row + 1, copiedVisit);
+			boolean flag = true;
+			for(Pos q : queens) {
+				if(col == q.c) flag = false;
+				else if((row - q.r) == Math.abs(col - q.c)) flag = false;
+			}
+			if(flag) {
+				queens.add(new Pos(row, col));
+				dfs(row + 1, queens);
+				queens.remove(row);
+			}
 		}
 	}
 	
-	private static void CantPutQueen(int row, int col, boolean[][] visit) {		// 다음 퀸이 놓을 수 없는 곳 체크
-		for(int i=0; i<n-row; i++) {
-			for(int j=-1; j<2; j++) 
-				if(row + i >= 0 && row + i < n && col + j*i >= 0 && col + j*i < n)
-					visit[row + i][col + j*i] = true;
+	static class Pos{
+		int r = 0;
+		int c = 0;
+		Pos(int r, int c){
+			this.r = r;
+			this.c = c;
 		}
 	}
-	
-	
-	
 }
