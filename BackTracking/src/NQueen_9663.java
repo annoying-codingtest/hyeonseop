7ee3fbe5f5d95fbda49232515fe94f168da2f244
johyunsub <gustjq2167@naver.com>
2021-10-07 00:28:36 +0900

    [Add] BOJ 9663

--- BackTracking/src/NQueen_9663.java
+++ BackTracking/src/NQueen_9663.java
@@ -5,44 +5,34 @@ import java.util.ArrayList;
 
 public class NQueen_9663 {
 	static int n, cnt;
-	static ArrayList<Pos> queens;
+	static int[] queens;	// 퀸의 좌표를 모아둘 배열(index는 퀸의 row값, value는 퀸의 col값)
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		queens = new ArrayList<Pos>(); 
-		for(int col=0; col<n; col++) {
-			queens.add(new Pos(0, col));
-			dfs(1, queens);
-			queens.remove(0);
+		queens = new int[n];
+		for(int col=0; col<n; col++) {		// (열탐색) 체스판에서 몇 번째 열에 퀸을 놓을지
+			queens[0] = col;				// 퀸의 시작 좌표(0,0) ~ (0,n) 
+			dfs(1);							// (dfs로 행탐색) 체스판에서 두번째 행부터 탐색 시작
 		}
 		System.out.println(cnt);
 	}
 
-	private static void dfs(int row, ArrayList<Pos> queens) {
+	private static void dfs(int row) {
 		if(n == row) {
 			cnt++;
 			return;
 		}
 		for(int col=0; col<n; col++) {
-			boolean flag = true;
-			for(Pos q : queens) {
-				if(col == q.c) flag = false;
-				else if((row - q.r) == Math.abs(col - q.c)) flag = false;
+			boolean isPutQueen = true;			// 퀸을 놓을 수 있는지, 최초 퀸을 놓을 수 있다고 가정(true) 
+			for(int i=0; i<row; i++) 			// 각 행마다 이미 놓여져 있는 퀸에 대해 조사
+				if(col == queens[i]) 			// i번째 행의 퀸과 같은 열에 있으면 퀸을 놓지 못하므로 isPutQueen : false
+					isPutQueen = false;
+				else if((row - i) == Math.abs(col - queens[i]))		// i번째 행의 퀸과 같은 대각선 방향이면 isPutQueen : false
+					isPutQueen = false;
+			if(isPutQueen) {			// 앞의 행들에 놓여져있는 퀸을 모두 피했다면 퀸을 놓을 준비 완료! 
+				queens[row] = col;		// (row, col)좌표에 퀸 set
+				dfs(row + 1);			// 다음 행에 놓을 퀸 조사
 			}
-			if(flag) {
-				queens.add(new Pos(row, col));
-				dfs(row + 1, queens);
-				queens.remove(row);
-			}
-		}
-	}
-	
-	static class Pos{
-		int r = 0;
-		int c = 0;
-		Pos(int r, int c){
-			this.r = r;
-			this.c = c;
 		}
 	}
 }
