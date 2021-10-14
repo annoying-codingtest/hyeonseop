3255aaf545343a683d1cbe4fb0bd913671d78742
johyunsub <gustjq2167@naver.com>
2021-10-14 23:55:40 +0900

    [Add] BOJ 10773

---
+++ Stack/src/제로_10773.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 제로_10773 {
+	static int n;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+
+	}
+}
