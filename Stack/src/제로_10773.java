10e93cc72b1cb4f276df7baaf23cecf3a8365021
johyunsub <gustjq2167@naver.com>
2021-10-15 23:30:57 +0900

    [Add] BOJ 10773

--- Stack/src/제로_10773.java
+++ Stack/src/제로_10773.java
@@ -3,10 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 제로_10773 {
-	static int n;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-
+		int n = Integer.parseInt(br.readLine());
+		int[] stack = new int[n];
+		int top = -1;
+		for(int i=0; i<n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(num != 0)	stack[++top] = num;
+			else	--top;			 
+		}
+		int sum = 0;
+		for(int i=0; i<=top; i++)
+			sum += stack[i];
+		System.out.println(sum);
 	}
 }
