2ecc672537ee312da71b69d3c40e7060f0e9720c
johyunsub <gustjq2167@naver.com>
2021-10-27 23:38:58 +0900

    [Add] BOJ 2630

--- DivideAndConquer/src/색종이만들기_2630.java
+++ DivideAndConquer/src/색종이만들기_2630.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class 색종이만들기_2630 {
-	static int n;
+	static int n, d = 0, whitePiece = 0, blackPiece = 0;
 	static int[][] grp;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +15,18 @@ public class 색종이만들기_2630 {
 			for(int j=0, k=0; j<n; j++, k+=2)
 				grp[i][j] = str.charAt(k) - '0';
 		}
-		
+		while(n < 1) {
+			n /= 2;
+			d++;
+		}
+		dfs(0);
+	}
+	
+	
+	private static void dfs(int depth) {
+		if(depth == d) {
+			
+		}
 		
 	}
 }
